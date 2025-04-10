6ae107ec6ce410dbf6482a2374620e678781f4db
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2025-04-10 21:24:57 +0700

    Update route.ts , add comment to entire file

--- src/app/api/roast/route.ts
+++ src/app/api/roast/route.ts
@@ -1,38 +1,38 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-const roastCache: { [key: string]: string } = {};
+// const roastCache: { [key: string]: string } = {};
 
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const username = searchParams.get("username");
+// export async function GET(req: Request) {
+//   const { searchParams } = new URL(req.url);
+//   const username = searchParams.get("username");
 
-  if (!username) {
-    return NextResponse.json(
-      { error: "Username is required" },
-      { status: 400 }
-    );
-  }
+//   if (!username) {
+//     return NextResponse.json(
+//       { error: "Username is required" },
+//       { status: 400 }
+//     );
+//   }
 
-  // Cek cache di server
-  if (roastCache[username]) {
-    return NextResponse.json({ username, roast: roastCache[username] });
-  }
+//   // Cek cache di server
+//   if (roastCache[username]) {
+//     return NextResponse.json({ username, roast: roastCache[username] });
+//   }
 
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BACKEND}/roast?username=${username}`
-    );
-    const data = await res.json();
+//   try {
+//     const res = await fetch(
+//       `${process.env.NEXT_PUBLIC_API_BACKEND}/roast?username=${username}`
+//     );
+//     const data = await res.json();
 
-    // Simpan ke cache
-    roastCache[username] = data.roast;
+//     // Simpan ke cache
+//     roastCache[username] = data.roast;
 
-    return NextResponse.json(data);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { error: "Failed to fetch roasting" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { error: "Failed to fetch roasting" },
+//       { status: 500 }
+//     );
+//   }
+// }
